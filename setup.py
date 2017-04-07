a7c6c6396567d3b2b1f97f8f40cfdfaa44137e19
Ghislain Picard <ghipicard@gmail.com>
2017-04-07 22:02:32 +0200

    first setup.py to install with pip

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup, Command
+
+
+setup(
+    name = "smrt",
+    packages = ["smrt"],
+    version = "0.9",
+    description = "Snow Microwave Radiative Transfer model",
+    author = "Ghislain Picard, Melody Sandells, Henning Loewe",
+    author_email = "ghislain.picard@univ-grenoble-alpes.fr, melody.sandells@gmail.com, loewe@slf.ch",
+    url = "https://github.com/smrt-model/smrt",
+    keywords = ["radiative transfer","model","snow","microwave"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        ],
+    long_description = """\
+The Snow Microwave Radiative Transfer (SMRT) model is a highly modular model to compute the thermal emission of snowpacks and other cryospheric bodies in the microwave domain.
+
+SMRT is compatible with Python 2.7 and 3.4+
+"""
+)
