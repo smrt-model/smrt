4e3c06d472651fe840299c4d46f5b52dbdf42833
Ghislain Picard <ghipicard@gmail.com>
2017-09-19 19:01:18 +0200

    the backscatter is not for all direction in phi. This is probably not a great solution, but this backscatter is anyway an adhoc hack.

--- smrt/substrate/reflector_backscatter.py
+++ smrt/substrate/reflector_backscatter.py
@@ -72,10 +72,12 @@ class Reflector(Substrate):
 
     def diffuse_reflection_matrix(self, m, frequency, eps_1, mu1, npol):
 
+        if m>0:
+            return 0
         if isinstance(self.backscattering_coefficient, dict):  # we have a dictionary with polarization
             diffuse_refl_coeff = np.empty(npol*len(mu1))
             coef = 1.0 if m > 0 else 0.5  # dirac to cosine series
-            coef /= mu1  # convert backscattering coefficient to intensity
+            coef /= mu1
             diffuse_refl_coeff[0::npol] += coef * self._get_refl(self.backscattering_coefficient['VV'], mu1)
             diffuse_refl_coeff[1::npol] += coef * self._get_refl(self.backscattering_coefficient['HH'], mu1)
         elif self.backscattering_coefficient is not None:
