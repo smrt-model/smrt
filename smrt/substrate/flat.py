da56819dfcdafe6602ad84a7286f2b80d831b629
Ghislain Picard <ghipicard@gmail.com>
2018-04-24 21:40:48 +0200

    wrong argument order

--- smrt/substrate/flat.py
+++ smrt/substrate/flat.py
@@ -17,11 +17,12 @@ class Flat(Substrate):
     args = []
     optional_args = {}
 
-    def specular_reflection_matrix(self, npol, frequency, eps_1, mu1, compute_coherent_only):
+    def specular_reflection_matrix(self, frequency, eps_1, mu1, npol, compute_coherent_only):
         """compute the specular reflection coefficients for an array of incidence angles (given by their cosine)
            in medium 1. Medium 2 is where the beam is transmitted.
 
         :param npol: number of polarization
+        :param frequency: frequency
         :param eps_1: permittivity of the medium where the incident beam is coming from.
         :param mu1: array of cosine of incident angles
 
@@ -29,9 +30,9 @@ class Flat(Substrate):
 """
         eps_2 = self.permittivity(frequency)
 
-        return fresnel_reflection_matrix(npol, eps_1, eps_2, mu1)
+        return fresnel_reflection_matrix(eps_1, eps_2, mu1, npol)
 
-    def absorption_matrix(self, npol, frequency, eps_1, mu1, compute_coherent_only):
+    def absorption_matrix(self, frequency, eps_1, mu1, npol, compute_coherent_only):
         """compute the absorption coefficients for an array of incidence angles (given by their cosine)
            in medium 1.
 
@@ -43,4 +44,4 @@ class Flat(Substrate):
 """
         eps_2 = self.permittivity(frequency)
 
-        return fresnel_transmission_matrix(npol, eps_1, eps_2, mu1)
+        return fresnel_transmission_matrix(eps_1, eps_2, mu1, npol)
