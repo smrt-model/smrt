5593200fa0cc5b3970350b89b564571793985fb4
Ghislain Picard <ghipicard@gmail.com>
2018-04-25 23:07:01 +0200

    change in the test to go along the commit on the argument order in flat.py

--- smrt/substrate/test_flat.py
+++ smrt/substrate/test_flat.py
@@ -5,4 +5,5 @@ from smrt.inputs.make_soil import make_soil
 
 def test_make_flat():
 
-    make_soil('flat', 'dobson85', 275, moisture=0.9, sand=0.2, clay=0.3, drymatter=1100)
+    sb = make_soil('flat', 'dobson85', 275, moisture=0.9, sand=0.2, clay=0.3, drymatter=1100)
+
