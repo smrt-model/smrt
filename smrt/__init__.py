5dbb29102df2be0cb8d7e3c0af5e350e53572166
Ghislain Picard <ghipicard@gmail.com>
2018-05-01 21:22:07 +0200

    add a depreciation warning for python 2

--- smrt/__init__.py
+++ smrt/__init__.py
@@ -2,6 +2,12 @@
 
 # functions to be exported by default
 
+import sys
+if sys.version_info[0] == 2:
+    import warnings
+    warnings.simplefilter('default')
+    warnings.warn("SMRT support of Python 2 is going to be dropped soon. Please contact us if this is a major issue for your application", PendingDeprecationWarning)
+
 
 from .inputs.make_medium import make_snowpack, make_snow_layer, make_ice_column
 from .inputs.make_soil import make_soil
