381c0fe05b7af1b3f1fe3746d21265b351e48f32
NinaMaass <nina.maass@uni-hamburg.de>
2018-05-17 17:23:18 +0200

    added snowpack on top of sea ice for IBA sea ice example code

--- examples/iba_sea_ice.py
+++ examples/iba_sea_ice.py
@@ -4,14 +4,12 @@ import numpy as np
 import sys
 
 # local import
-from smrt import make_ice_column, make_model, sensor_list
+from smrt import make_ice_column, make_snowpack, make_model, sensor_list
 from smrt import PSU
 #from smrt.inputs.make_medium import make_ice_column
 
 # prepare inputs
 l = 9 #9 ice layers
-n_max_stream = 64
-
 thickness = np.array([1.5/l] * l) #ice is 1.5m thick
 p_ex = np.array([5.e-4] * (l)) #correlation length
 temperature = np.linspace(273.15-20., 273.15 - 1.8, l) #temperature gradient in the ice from -20 deg C at top to freezing temperature of water at bottom (-1.8 deg C)
@@ -34,13 +32,37 @@ ice_column = make_ice_column(thickness=thickness,
 #Optional arguments for function make_ice_column() are 'water_temperature' (default = -1.8degC),
 #'water_salinity' (default = 32.) and 'water_depth' (default = 10m, i.e. infinitely thick for microwave radiation) of the water layer.
 
+################################################################################################
+
+# create snowpack with two snow layers:
+l_s=2 #2 snow layers
+thickness_s = np.array([0.05, 0.2])
+p_ex_s = np.array([5e-5]*l_s)
+temperature_s = np.linspace(273.15-25., 273.15 - 20, l_s)
+density_s = [200, 340]
+
+# create the snowpack
+snowpack = make_snowpack(thickness=thickness_s,
+                         microstructure_model="exponential",
+                         density=density_s,
+                         temperature=temperature_s,
+                         corr_length=p_ex_s)
+
+#add snowpack on top of ice column:
+medium = snowpack + ice_column
+
 # create the sensor
 sensor = sensor_list.passive(1.4e9, 40.)
 
-m = make_model("iba", "dort")
+n_max_stream = 128 #TB calculation is more accurate if number of streams is increased (currently: default = 32);
+#needs to be increased when using > 1 snow layer on top of sea ice! 
+m = make_model("iba", "dort", rtsolver_options ={"n_max_stream": n_max_stream})
 
-# run the model
-res = m.run(sensor, ice_column)
+# run the model for bare sea ice:
+res1 = m.run(sensor, ice_column)
+# run the model for snow-covered sea ice:
+res2 = m.run(sensor, medium)
 
 # print TBs at horizontal and vertical polarization:
-print(res.TbH(), res.TbV())
+print(res1.TbH(), res1.TbV())
+print(res2.TbH(), res2.TbV())
