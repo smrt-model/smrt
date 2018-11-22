0380766de620140a1b2522839d1ff84606f3f80d
Ghislain Picard <ghislain.picard@univ-grenoble-alpes.fr>
2018-11-22 08:36:54 +0100

    The default permittivity for snow is now wetsnow, which works the same for dry snow as maetlzer06 (compatible with previous version), but automatically add support for wetsnow.

--- smrt/inputs/make_medium.py
+++ smrt/inputs/make_medium.py
@@ -103,8 +103,8 @@ def make_snow_layer(layer_thickness, microstructure_model,
 
     if ice_permittivity_model is None:
         # must import this here instead of the top of the file because of cross-dependencies
-        from ..permittivity.ice import ice_permittivity_maetzler06  # default ice permittivity model
-        ice_permittivity_model = ice_permittivity_maetzler06
+        from ..permittivity.wetsnow import wetsnow_permittivity  # default ice permittivity model, use ice_permittivity_maetzler06 for dry snow and add support for wet snow
+        ice_permittivity_model = wetsnow_permittivity
 
     # ice in air background. Note that the emmodel might inverse the medium or use other technique for mid-range densities.
     # This is the case of DMRT_Shortrange for instance.
