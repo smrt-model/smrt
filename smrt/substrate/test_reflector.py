e8fe307a8d8307e1a1b49db839a731cf1ff96a51
Mel Sandells <melody.sandells@coresscience.co.uk>
2020-04-28 13:09:30 +0100

    Allows reflector to be specified by frequency and polarization

--- smrt/substrate/test_reflector.py
+++ smrt/substrate/test_reflector.py
@@ -1,6 +1,9 @@
 
 
 import numpy as np
+from nose.tools import raises
+
+from smrt.core.error import SMRTError
 from smrt.substrate.reflector import Reflector
 
 
@@ -37,3 +40,49 @@ def test_func_specular():
 
     assert np.all(m[0] == 0.5)
     assert np.all(m[1] == 0.5)
+
+
+def test_dict_multifrequency():
+
+    refl = Reflector(specular_reflection={21e9: 0.5, 36e9: 0.7})
+    m1 = refl.specular_reflection_matrix(21e9, None, mu, 2)
+    m2 = refl.specular_reflection_matrix(36e9, None, mu, 2)
+
+    assert np.all(m1[0] == 0.5)
+    assert np.all(m2[1] == 0.7)
+
+
+@raises(SMRTError)
+def test_missing_frequency_warning():
+    refl = Reflector(specular_reflection={21e9: 0.5, 36e9: 0.7})
+    m1 = refl.specular_reflection_matrix(10e9, None, mu, 2)
+
+
+def test_emissivity_reflectivity_relation():
+    refl = Reflector(specular_reflection={(21e9, 'H'): 0.5, (21e9, 'V'): 0.6, (36e9, 'H'): 0.7, (36e9, 'V'): 0.8})
+    r = refl.specular_reflection_matrix(21e9, None, mu, 2)
+    e = refl.emissivity_matrix(21e9, None, mu, 2)
+    assert np.all((r + e).values == 1.)
+
+
+def test_tuple_dict_multifrequency():
+
+    refl = Reflector(specular_reflection={(21e9, 'H'): 0.5, (21e9, 'V'): 0.6, (36e9, 'H'): 0.7, (36e9, 'V'): 0.8})
+    m1 = refl.specular_reflection_matrix(21e9, None, mu, 2)
+    m2 = refl.specular_reflection_matrix(36e9, None, mu, 2)
+
+    assert np.all(m1[1] == 0.5)
+    assert np.all(m1[0] == 0.6)
+    assert np.all(m2[1] == 0.7)
+    assert np.all(m2[0] == 0.8)
+
+
+def test_inverted_reflector_dictionary():
+    refl = Reflector(specular_reflection={('H', 21e9): 0.5, ('V', 21e9): 0.6, (36e9, 'H'): 0.7, (36e9, 'V'): 0.8})
+    m1 = refl.specular_reflection_matrix(21e9, None, mu, 2)
+    m2 = refl.specular_reflection_matrix(36e9, None, mu, 2)
+
+    assert np.all(m1[1] == 0.5)
+    assert np.all(m1[0] == 0.6)
+    assert np.all(m2[1] == 0.7)
+    assert np.all(m2[0] == 0.8)
