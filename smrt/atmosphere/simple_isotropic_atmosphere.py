7a759c85b93655bcb67f92dee4d54f6da0de8e94
Mel Sandells <melody.sandells@coresscience.co.uk>
2020-05-29 16:56:46 +0100

    Allows frequency-dependent atmosphere

--- smrt/atmosphere/simple_isotropic_atmosphere.py
+++ smrt/atmosphere/simple_isotropic_atmosphere.py
@@ -1,5 +1,21 @@
 # coding: utf-8
-"""Implement an isotropic atmosphere with prescribed emission (up and down) and transmittivity
+"""Implement an isotropic atmosphere with prescribed frequency-dependent emission (up and down) and transmittivity.
+
+ TB and transmissivity can be specified as a constant, or a frequency-dependent dictionary
+
+ To make an atmosphere, it is recommended to use the helper function :py:func:`~smrt.atmosphere.simple_isotropic_atmosphere.make_atmosphere`.
+
+
+Examples::
+
+    # the full path import is required
+    from smrt.atmosphere.simple_isotropic_atmosphere import make_atmosphere
+
+    # Constant
+    atmos = make_atmosphere(tbdown=20., tbup=6., trans=1)
+
+    # Frequency-dependent
+    atmos = make_atmosphere(tbdown={10e9: 15.2, 21e9: 23.5})
 
 """
 
@@ -10,15 +26,23 @@
 import numpy as np
 
 # local import
-#from ..core.error import SMRTError
 
 
 #
 # For the developers:
-# A valid Atmosphere is any object that provide a tbdown, tbup and trans functions function of frequency and costheta.
+# A valid atmosphere is any object that provide a tbdown, tbup and trans functions function of frequency and costheta.
 # costheta can be an array or a value, both must be handled.
-# The implementation below is independent of the frequency and the propagation angle
+# The implementation below is independent of the propagation angle
 #
+
+def make_atmosphere(tbdown=0, tbup=0, trans=1):
+
+    """ Construct an atmosphere instance.
+
+    """
+
+    # create the instance
+    return SimpleIsotropicAtmosphere(tbdown=tbdown, tbup=tbup, trans=trans)
 
 
 class SimpleIsotropicAtmosphere(object):
@@ -30,11 +54,19 @@ class SimpleIsotropicAtmosphere(object):
         self.constant_trans = trans
 
     def tbdown(self, frequency, costheta, npol):
-        return np.full(len(costheta)*npol, self.constant_tbdown, dtype=np.float32)
+        if isinstance(self.constant_tbdown, dict):
+            return np.full(len(costheta) * npol, self.constant_tbdown[frequency], dtype=np.float32)
+        else:
+            return np.full(len(costheta) * npol, self.constant_tbdown, dtype=np.float32)
 
     def tbup(self, frequency, costheta, npol):
-        return np.full(len(costheta)*npol, self.constant_tbup, dtype=np.float32)
+        if isinstance(self.constant_tbup, dict):
+            return np.full(len(costheta) * npol, self.constant_tbup[frequency], dtype=np.float32)
+        else:
+            return np.full(len(costheta) * npol, self.constant_tbup, dtype=np.float32)
 
     def trans(self, frequency, costheta, npol):
-        return np.full(len(costheta)*npol, self.constant_trans, dtype=np.float32)
-
+        if isinstance(self.constant_trans, dict):
+            return np.full(len(costheta) * npol, self.constant_trans[frequency], dtype=np.float32)
+        else:
+            return np.full(len(costheta) * npol, self.constant_trans, dtype=np.float32)
