6f4c6f205ddde283a49ddb43b2c6324b4ae03844
Ghislain Picard <ghislain.picard@univ-grenoble-alpes.fr>
2020-12-14 18:30:34 +0100

    improved formulation for SHS

--- smrt/microstructure_model/unified_sticky_hard_spheres.py
+++ smrt/microstructure_model/unified_sticky_hard_spheres.py
@@ -24,6 +24,11 @@ class UnifiedStickyHardSpheres(UnifiedAutocorrelation):
 
         self.radius = 3 / 4 * self.porod_length / (1 - self.frac_volume)
 
+        K_32 = self.polydispersity**(-3 / 2)
+        self.t = (1 + 2 * self.frac_volume - 3 / (8 * np.sqrt(2)) * K_32) / self.corr_func_at_origin
+
+        # t must check the condition t * f (1-f) < 1 + 2*f  which seems to be always valid...
+
     def basic_check(self):
         """check consistency between the parameters"""
         pass
@@ -31,6 +36,10 @@ class UnifiedStickyHardSpheres(UnifiedAutocorrelation):
     # No analytical function exists
     # def autocorrelation_function(self, r):
     #
+    def compute_stickiness(self):
+
+        phi_2 = self.frac_volume
+        return phi_2 / 12 * self.t - phi_2 / (1 - phi_2) + (1 + phi_2 / 2) / (self.t * (1 - phi_2)**2)
 
     def ft_autocorrelation_function(self, k):
         """Compute the 3D Fourier transform of the isotropic correlation
@@ -48,9 +57,6 @@ class UnifiedStickyHardSpheres(UnifiedAutocorrelation):
 
         d = 2 * self.radius
         phi_2 = self.frac_volume
-
-        K_32 = self.polydispersity**(-3 / 2)
-        t = (1 + 2 * self.frac_volume - 3 / (8 * np.sqrt(2)) * K_32) / self.corr_func_at_origin
 
         # scaling variable, Eq 32, LP2015
         X = np.atleast_1d(k) * d / 2.0
@@ -77,8 +83,8 @@ class UnifiedStickyHardSpheres(UnifiedAutocorrelation):
         Phi_tsang_vol = 1.0 / vd  # Ghislain says: after simple math, vint sqrt_vint_X2 simplifies
 
         # auxiliary quantities Eq 31, LP2015
-        A_tsang_vol = (phi_2 / (1 - phi_2) * ((1 - t * phi_2 + 3 * phi_2 / (1 - phi_2)) * Phi_tsang_vol +
-                      (3 - t * (1 - phi_2)) * Psi_tsang_vol) + np.cos(X) / sqrt_vint)
+        A_tsang_vol = (phi_2 / (1 - phi_2) * ((1 - self.t * phi_2 + 3 * phi_2 / (1 - phi_2)) * Phi_tsang_vol +
+                                              (3 - self.t * (1 - phi_2)) * Psi_tsang_vol) + np.cos(X) / sqrt_vint)
         B_tsang_vol = phi_2 / (1 - phi_2) * X * Phi_tsang_vol + np.sin(X) / sqrt_vint
 
         # structure factor Eq 31, LP2015
@@ -90,6 +96,7 @@ class UnifiedStickyHardSpheres(UnifiedAutocorrelation):
         # set limit value at k=0 manually, Eq. 33, LP2015
         # zerok = np.isclose(X, 0)
         # Ctilde[zerok] = (n * vd**2 / (phi_2 / (1-phi_2) * ((1 - t*phi_2 + 3 * phi_2 / (1 - phi_2)) + (3 - t * (1 - phi_2))) + 1)**2)
-        Ctilde[zerok] = phi_2 * vd / (phi_2 / (1 - phi_2) * ((1 - t * phi_2 + 3 * phi_2 / (1 - phi_2)) + (3 - t * (1 - phi_2))) + 1)**2
+        Ctilde[zerok] = phi_2 * vd / (phi_2 / (1 - phi_2) * (
+            (1 - self.t * phi_2 + 3 * phi_2 / (1 - phi_2)) + (3 - self.t * (1 - phi_2))) + 1)**2
 
         return Ctilde
