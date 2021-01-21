3ae818a4ad5163596e506078aaacd6780b2e4da3
Mel Sandells <melody.sandells@northumbria.ac.uk>
2021-01-21 18:30:20 +0000

    Adds Choudhury 1979 roughness model to substrate

---
+++ smrt/substrate/rough_choudhury79.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+
+"""Implement the rough boundary reflectivity presented in Choudhury et al. (1979). It is not suitable for the active mode.
+
+Applicable for ksigma<<1
+
+parameters: roughness_rms
+
+"""
+
+import numpy as np
+
+# local import
+from smrt.core.interface import Substrate
+from smrt.core.fresnel import fresnel_reflection_matrix, fresnel_transmission_matrix
+from smrt.core import lib
+
+
+class ChoudhuryReflectivity(Substrate):
+
+    args = ['roughness_rms']
+    optional_args = {}
+
+    def adjust(self, rh, rv, frequency, eps_1, mu1):
+        # in place modification of rh and rv for the rough reflectivity model of Choudhury et al. (1979)
+
+        #  Calculate ksigma = wavenumber*soilp%sigma(standard deviation of surface height)
+
+        ksigma = 2 * np.pi * frequency * np.sqrt((1 / 2.9979e8)**2 * eps_1) * self.roughness_rms
+        ksigma = ksigma.real
+
+        # Raise warning if outside validity
+        if ksigma > 0.1:
+            raise Warning("Reflectivity may be outside validity range. ksigma should be << 1")
+
+        #  Calculation of rh with ksigma
+        rh *= np.exp(-4 * ksigma**2 * mu1**2)  # H pola
+        rv *= np.exp(-4 * ksigma**2 * mu1**2)  # V pola
+
+    def specular_reflection_matrix(self, frequency, eps_1, mu1, npol):
+
+        eps_2 = self.permittivity(frequency)
+
+        reflection_coefficients = fresnel_reflection_matrix(eps_1, eps_2, mu1, npol)
+
+        self.adjust(reflection_coefficients[1], reflection_coefficients[0], frequency, eps_1, mu1)
+
+        if npol >= 3:
+            # don't modify the third compoment... this is an approximation, as the third component should be affected by the roughness...
+            # don't use this model for active mode
+            pass
+        if npol == 4:
+            raise NotImplementedError("to be implemented, the matrix is not diagonal anymore")
+
+        return reflection_coefficients
+
+    def emissivity_matrix(self, frequency, eps_1, mu1, npol):
+
+        # this function is a bit complex because we have to change first and second component but not the third one.
+        # this is an approximation, as the third component should be affected by the roughness...
+
+        eps_2 = self.permittivity(frequency)
+
+        transmission_coefficients = fresnel_transmission_matrix(eps_1, eps_2, mu1, npol)
+
+        rh = 1 - transmission_coefficients[1]
+        rv = 1 - transmission_coefficients[0]
+
+        self.adjust(rh, rv, frequency, eps_1, mu1)
+
+        transmission_coefficients[1] = 1 - rh  # back to transmission coefficients
+        transmission_coefficients[0] = 1 - rv
+
+        if npol >= 3:
+            # don't modify the third compoment... don't know what to do with it !
+            pass
+        if npol == 4:
+            raise NotImplementedError("to be implemented, the matrix is not diagonal anymore")
+
+        return transmission_coefficients
---
+++ smrt/substrate/test_rough_choudhury79.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+import pytest
+
+from smrt.inputs.make_soil import make_soil
+from smrt.permittivity.water import water_permittivity
+
+
+def test_make_rough_choudhury():
+
+    make_soil('rough_choudhury79', 'dobson85', 275, moisture=0.9, sand=0.2, clay=0.3, drymatter=1100, roughness_rms=1e-2)
+
+
+def test_rough_choudhury_reflection():
+
+    s = make_soil('rough_choudhury79', 'dobson85', 275, moisture=0.9, sand=0.2, clay=0.3, drymatter=1100, roughness_rms=1e-3)
+
+    frequency = 1e9
+    mu1 = np.cos(np.radians(np.arange(10, 80)))
+
+    npol = 2
+
+    s.specular_reflection_matrix(frequency, 1, mu1, npol)
+
+
+def test_raises_ksigma_warning():
+    with pytest.raises(Warning):
+        s = make_soil('rough_choudhury79', 'dobson85', 275, moisture=0.9, sand=0.2, clay=0.3, drymatter=1100, roughness_rms=0.1)
+        frequency = 1e9
+        mu1 = np.cos(np.radians(np.arange(10, 80)))
+        npol = 2
+        s.specular_reflection_matrix(frequency, 1, mu1, npol)
+
+
+def test_make_rough_water():
+    make_soil('rough_choudhury79', water_permittivity(6e9, 273), temperature=270, roughness_rms=0.5e-3)
+
+
+def test_equivalence_fresnel():
+
+    h2o = water_permittivity(6e9, 273)
+    rough = make_soil('rough_choudhury79', h2o, temperature=273, roughness_rms=0.)
+    smooth = make_soil('flat', h2o, temperature=273)
+    np.testing.assert_equal(rough.specular_reflection_matrix(10e9, 1, np.cos(50), 2)[0], smooth.specular_reflection_matrix(10e9, 1, np.cos(50), 2)[0])
