6a8888c786a9b84e3f37e61a605e7d5b6c4ab7c3
carlomarin <camabagi@gmail.com>
2022-03-31 15:26:08 +0200

    Distinction between H86 and H86_Ulaby14
    Distinction between H86 and H86_Ulaby14 in two different functions. Documentation of the updates.

--- smrt/permittivity/snow_mixing_formula.py
+++ smrt/permittivity/snow_mixing_formula.py
@@ -16,7 +16,7 @@ the already mixed materials with the background material).
 import numpy as np
 from ..core.layer import layer_properties
 from ..core.globalconstants import FREEZING_POINT, DENSITY_OF_ICE, DENSITY_OF_WATER
-from ..core.error import SMRTError
+from ..core.error import SMRTError, SMRTWarning
 from .generic_mixing_formula import polder_van_santen, polder_van_santen_three_components, polder_van_santen_three_spherical_components
 
 
@@ -172,10 +172,61 @@ def wetsnow_permittivity_hallikainen86(frequency, density, liquid_water):
     The validity of the model is: frequency between 3 and 37GHz;
                                   mv between 1% and 12%;
                                   dry_snow_density between 0.09 and 0.38g/cm3.
+                                  
+    The implementation of this function follows the equations formulation of the original paper 
+    Hallikainen, M., F. Ulaby, and M. Abdelrazik, “Dielectric properties of snow in 3 to 37 GHz range,”
+    IEEE Trans. on Antennasand Propagation,Vol. 34, No. 11, 1329–1340, 1986. DOI: 10.1109/TAP.1986.1143757
+    Anyway this formulation does not allow the reproduction of the results as reported in the paper.
+    A new formulation of eq. 12a have been presented in the book
+    Microwave Radar and Radiometric Remote Sensing by Ulaby et al. 2014 from which the SMRT function 
+    wetsnow_permittivity_hallikainen86_ulaby14 have been implemented. The users are pointed to that definition.
+    
 
-Hallikainen, M., F. Ulaby, and M. Abdelrazik, “Dielectric properties of snow in 3 to 37 GHz range,”
-IEEE Trans. on Antennasand Propagation,Vol. 34, No. 11, 1329–1340, 1986. DOI: 10.1109/TAP.1986.1143757
+    """
+    
+    raise SMRTWarning("This model cannot reproduce the results of the original paper. You may want to use wetsnow_permittivity_hallikainen86_ulaby14 instead.")
+    
+    frac_volume, fi, fw = compute_frac_volumes(density, liquid_water)
 
+    # fractional volume of water in %
+    mv = 100 * fw
+
+    # Eq 3 in H86 defines the dry snow by (snow density - mass of water per volume of snow) / (1 - volume fo water per volume of snow)
+    dry_snow_density_gcm3 = 1e-3 * (density - DENSITY_OF_WATER * fw) / (1 - fw)
+
+    freqGHz = frequency * 1e-9
+    
+    # Eq 13
+    A1 = 0.78 + 0.03 * freqGHz - 0.58e-3 * freqGHz**2
+    A2 = 0.97 - 0.39e-2 * freqGHz + 0.39e-3 * freqGHz**2
+    B1 = 0.31 - 0.05 * freqGHz + 0.87e-3 * freqGHz**2  
+
+    # Eq 12 (different from Ulaby14)
+    A = 1 + 1.83 * dry_snow_density_gcm3 + 0.02 * A1 * mv**1.015 + B1
+    B = 0.073 * A1
+    C = 0.073 * A2
+    x = 1.31
+
+    freq0 = 9.07  # GHz
+
+    eps_ws_r = A + B * mv**x / (1 + (freqGHz / freq0)**2)
+
+    eps_ws_i = C * mv**x * (freqGHz / freq0) / (1 + (freqGHz / freq0)**2)
+
+    return eps_ws_r + 1j * eps_ws_i
+
+
+@layer_properties("density", "liquid_water")
+def wetsnow_permittivity_hallikainen86_ulaby14(frequency, density, liquid_water):
+    """effective permittivity of a snow mixture calculated with the Modified Debye model by Hallikainen 1986
+    and revised in Microwave Radar and Radiometric Remote Sensing by Ulaby et al. 2014 
+    Equations implemented are ch 4 pp 143-15 4.60a - 4.61h.
+    
+    The validity of the model is: frequency between 3 and 37GHz;
+                                  mv between 1% and 12%;
+                                  dry_snow_density between 0.09 and 0.38g/cm3.
+    
+    Same formulation can be reproduced by the book code https://mrs.eecs.umich.edu/codes/Module4_6/Module4_6.html
     """
 
     frac_volume, fi, fw = compute_frac_volumes(density, liquid_water)
@@ -188,24 +239,25 @@ IEEE Trans. on Antennasand Propagation,Vol. 34, No. 11, 1329–1340, 1986. DOI: 
 
     freqGHz = frequency * 1e-9
     
-    if (mv > 12 or dry_snow_density_gcm3 < 0.09 or dry_snow_density_gcm3 > 0.38 or freqGHz <3 or freqGHz > 37):
-        raise SMRTError("Out of validity of the model.")
+    if np.any(mv > 12) \
+    or np.any(dry_snow_density_gcm3 < 0.09) or np.any(dry_snow_density_gcm3 > 0.38) \
+    or np.any(freqGHz < 3) or np.any(freqGHz > 37):
+       raise SMRTWarning("Hallikainen86_ulaby14 is only valid for mv < 12  dry_snow_density in [0.09...0.38] and freq in [3...37 GHz]")  
 
-    # Eq 13
+    # Eq 4.61 f - h
     A1 = 0.78 + 0.03 * freqGHz - 0.58e-3 * freqGHz**2
     A2 = 0.97 - 0.39e-2 * freqGHz + 0.39e-3 * freqGHz**2
     B1 = 0.31 - 0.05 * freqGHz + 0.87e-3 * freqGHz**2   # <-- this leads to a too low epsilon compared to the H86 graphs
 
-    # Eq 12
+    # Eq 4.61 a - e
     A = A1 * (1.0 + 1.83 * dry_snow_density_gcm3 + 0.02 * mv**1.015) + B1
     B = 0.073 * A1
     C = 0.073 * A2
     x = 1.31
-
     freq0 = 9.07  # GHz
 
+    # Eq 4.60 a - b
     eps_ws_r = A + B * mv**x / (1 + (freqGHz / freq0)**2)
-
     eps_ws_i = C * mv**x * (freqGHz / freq0) / (1 + (freqGHz / freq0)**2)
 
     return eps_ws_r + 1j * eps_ws_i
