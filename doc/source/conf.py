17b93908a43e45dc0a32faadf3e7522c28ac4aa1
Mel Sandells <melody.sandells@northumbria.ac.uk>
2022-07-25 15:20:30 +0100

    Change theme to karma_sphinx_theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -69,9 +69,9 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
-    'sphinx.ext.autosummary',
-    'python_docs_theme'
-]
+    'sphinx.ext.autosummary'
+ ]
+
 autosummary_generate = True
 templates_path = [ '_templates' ]
 exclude_patterns = ['_build', '_templates']
@@ -175,7 +175,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'bizstyle'
-html_theme = 'python_docs_theme'
+html_theme = 'karma_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
