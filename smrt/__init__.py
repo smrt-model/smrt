5c43dbd3a9651336996f673e1c18d058b3084b02
Ghislain Picard <ghislain.picard@univ-grenoble-alpes.fr>
2022-08-12 23:41:08 +0200

    add make_water_body in the default imports

--- smrt/__init__.py
+++ smrt/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info[0] == 2:
     raise RuntimeError("Pyhton 2.7 is not supported anymore")
 
 
-from .inputs.make_medium import make_snowpack, make_snow_layer, make_ice_column, make_atmosphere, make_interface
+from .inputs.make_medium import make_snowpack, make_snow_layer, make_ice_column, make_atmosphere, make_interface, make_water_body
 from .inputs.make_soil import make_soil
 
 from .core.model import make_model, make_emmodel
