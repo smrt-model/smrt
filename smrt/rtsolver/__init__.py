ca54b82ef71599f6fb84b6bde5a0fff499abdded
Ghislain Picard <ghislain.picard@univ-grenoble-alpes.fr>
2025-06-30 22:16:36 +0200

    update the documentation with a recommendation table for the available solvers

--- smrt/rtsolver/__init__.py
+++ smrt/rtsolver/__init__.py
@@ -8,6 +8,34 @@ in most cases unless the computation time is a constraint.
 
 Selection of the solver is done with the :py:mod:`smrt.core.model.make_model` function.
 
+
+Here are some recommendations to chose the appropriate solver:
+
+.. list-table:: SMRT Solvers Overview
+   :header-rows: 1
+   :widths: 20 50 30 20
+
+   * - Solver Name
+     - Description
+     - Use Case
+     - Performance
+   * - ``dort``
+     - The default original solver in SMRT for passive microwave (brightness temperature) and radar (sigma0). General and robust.
+     - General applications for both passive microwave and radar.
+     - Robust but slow
+   * - ``iterative_first_order``
+     - Radar-only solver using an iterative method up to first order. Fast and provides contributions of interaction mechanisms.
+     - Radar studies, especially to analyze interaction mechanisms.
+     - Fast
+   * - ``multifresnel_thermalemission``
+     - Passive microwave-only solver assuming no scattering in the layers and flat interfaces.
+     - Radiometry at low frequency when scattering is negligible.
+     - Very fast
+   * - ``nadir_lrm_altimetry``
+     - Specialized solver for altimeter in Low Rate Mode. Solve the first order interaction.
+     - Altimetry in Low Rate Mode (LRM).
+     -
+
 Note:  **For Developers**
 
     To experiment with DORT, it is recommended to copy the file dort.py to e.g. dort_mytest.py so it is immediately available through
