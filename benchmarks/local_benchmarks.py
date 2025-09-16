777ca685f35cdd2b3cb10f80b1177303e3899245
Hippolyte Signargout <hippolyte.signargout@univ-grenoble-alpes.fr>
2025-09-16 14:49:55 +0200

    Aggregate benchmarks. Small fixes. Change results directory.

--- benchmarks/local_benchmarks.py
+++ benchmarks/local_benchmarks.py
@@ -8,99 +8,75 @@ from smrt.microstructure_model.sticky_hard_spheres import StickyHardSpheres
 from smrt.core.error import SMRTWarning
 import warnings
 
-class Multiple_Snowpacks:
+class MultipleSemiInfinite:
     """
     Benchmark time and peak memory for creating multiple snowpacks and running model in parrallel
 
     Inspired from smrt/test/test_model.py
     """
-    param_names = ["snowpack_amount"]
-    params = [1000] #Add values for multiple benchmarks
-    def setup(self, n):
+    param_names = ["n_snowpack"]
+    params = [500] #Add values for multiple benchmarks
+    def setup(self, n_snowpack):
         """
-        Initiates snowpacks, model and sensor for the benchmarks
+        Initiates model and sensor for the benchmarks
         """
-        self.temperatures = np.linspace(200, 270, n)
-        self.snowpack_list = [make_snowpack([2000], StickyHardSpheres, density=[250], temperature=t,
-                                            radius=0.3e-3, stickiness=0.2) for t in self.temperatures]
-        self.m = Model('dmrt_qcacp_shortrange', DORT)
+        self.temperature_list = np.linspace(200, 270, n_snowpack)
+        self.radius_list = np.linspace(5e-5, 1e-3, n_snowpack) 
+        self.m = Model('iba', "dort")
         self.sensor = amsre()
+    
+    def set_and_run(self, n_snowpack):
+        self.snowpack_list = [make_snowpack([2000], StickyHardSpheres, density=[250],
+                                            temperature=self.temperature_list[i],
+                                            radius= self.radius_list[i], stickiness=0.2) for i in range(n_snowpack)]
+        self.m.run(self.sensor, self.snowpack_list, parallel_computation=True)
 
-    def time_setup(self, n):
+    def time_setrun(self, n_snowpacks):
         """
-        Benchmark time for creating snowpack list
+        Benchmark time for creating and running snowpack list
         """
-        [make_snowpack([2000], StickyHardSpheres, density=[250], temperature=t,
-                                            radius=0.3e-3, stickiness=0.2) 
-                                            for t in self.temperatures]
+        self.set_and_run(n_snowpacks)
         
-    def peakmem_setup(self, n):
+    def peakmem_setrun(self, n_snowpacks):
         """
         Benchmark memory usage for creating snowpack list
         """
-        [make_snowpack([2000], StickyHardSpheres, density=[250], temperature=t,
-                                            radius=0.3e-3, stickiness=0.2)
-                                            for t in self.temperatures]
+        self.set_and_run(n_snowpacks)
 
-    def time_parrallel_run(self, n):
-        """
-        Benchmark time for running model
-        """
-        self.m.run(self.sensor, self.snowpack_list, parallel_computation=True)
-
-    def peakmem_parrallel_run(self, n):
-        """
-        Benchmark memory usage for running model
-        """
-        self.m.run(self.sensor, self.snowpack_list, parallel_computation=True)
-
-class Large_Snowpack:
+class SemiInfiniteMultipleLayer:
     """
     Benchmark time and peak memory for creating a large snowpack and running model
 
     Inspired from smrt/test/test_coherent_layer.py
     """
-    param_names = ["layer_amount"]
-    params = [600] #Add values for multiple benchmarks
-    def setup(self, layer_amount):
+    param_names = ["n_layer"]
+    params = [300] #Add values for multiple benchmarks
+    def setup(self, n_layer):
         """
         Initiates snowpack, model and sensor for the benchmarks
         """
-         # this test is only here to avoid regression, it is not scientifically validated
-
-        self.density = np.linspace(300, 916.7, layer_amount)
-        self.thickness = np.ones(layer_amount) * 3000 / layer_amount
-        self.temperature = np.linspace(200, 270, layer_amount)
-        self.corr_length = np.ones(layer_amount) *200e-6
+        self.density = np.linspace(300, 916.7, n_layer)
+        self.thickness = np.ones(n_layer) * 3000 / n_layer
+        self.temperature = np.linspace(200, 270, n_layer)
+        self.corr_length = np.ones(n_layer) *200e-6
         theta = 60
         self.radiometer = sensor_list.passive(5e9, theta)
-        self.sp = make_snowpack(self.thickness, "exponential", density=self.density,
-                                temperature=self.temperature, corr_length=self.corr_length)
         # create the EM Model - Equivalent DMRTML
         self.m = make_model("iba", "dort", rtsolver_options={"n_max_stream": 64, "process_coherent_layers": True})
         warnings.simplefilter("ignore", category=SMRTWarning)
 
-    def time_setup(self, layer_amount):
+    def set_run(self, n_layer):
+        self.sp = make_snowpack(self.thickness, "exponential", density=self.density,
+                                temperature=self.temperature, corr_length=self.corr_length)
+        self.m.run(self.radiometer, self.sp, parallel_computation=True)
+    def time_setrun(self, n_layer):
         """
         Benchmark time for creating snowpack list
         """
-        make_snowpack(self.thickness, "exponential", density=self.density, temperature=self.temperature, corr_length=self.corr_length)
+        self.set_run(n_layer)
         
-    def peakmem_setup(self, layer_amount):
+    def peakmem_setrun(self, n_layer):
         """
         Benchmark memory usage for creating snowpack list
         """
-        make_snowpack(self.thickness, "exponential", density=self.density,
-                      temperature=self.temperature, corr_length=self.corr_length)
-
-    def time_parrallel_run(self, layer_amount):
-        """
-        Benchmark time for running model
-        """
-        self.m.run(self.radiometer, self.sp, parallel_computation=True)
-
-    def peakmem_parrallel_run(self, layer_amount):
-        """
-        Benchmark memory usage for running model
-        """
-        self.m.run(self.radiometer, self.sp, parallel_computation=True)
+        self.set_run(n_layer)
