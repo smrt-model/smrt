243def73db5056a153b4bde77b9b30e2bb06f943
Hippolyte Signargout <hippolyte.signargout@univ-grenoble-alpes.fr>
2025-10-31 15:30:17 +0000

    Factorise tests and use fixtures in permittivity, inputs, interface, microstructure_model. Remains emmodel (difficult), rtsolver, runner, substrate and test.

--- smrt/permittivity/test_depolarization_factors,.py
+++ smrt/permittivity/test_depolarization_factors,.py
@@ -1,3 +1,4 @@
+import pytest
 from .depolarization_factors import depolarization_factors_spheroids
 
 high_tolerance = 1e-8
@@ -19,12 +20,8 @@ def test_hoar_columns_depol():
     depol = depolarization_factors_spheroids(length_ratio=0.5)
     assert depol[0] < depol[2]
 
-
-def test_depol_approach_to_isotropy_above():
-    depol = depolarization_factors_spheroids(length_ratio=1.01)
+@pytest.mark.parametrize("length_ratio", [1.01, 0.99])
+def test_depol_approach_to_isotropy_above(length_ratio):
+    depol = depolarization_factors_spheroids(length_ratio=length_ratio)
     assert abs(depol[0] - (1.0 / 3.0)) < low_tolerance
 
-
-def test_depol_approach_to_isotropy_below():
-    depol = depolarization_factors_spheroids(length_ratio=0.99)
-    assert abs(depol[0] - (1.0 / 3.0)) < low_tolerance
