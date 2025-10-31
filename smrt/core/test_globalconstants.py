5c5e1b42978ccb1b51f6e18bb3e51264f6510dcf
Hippolyte Signargout <hippolyte.signargout@univ-grenoble-alpes.fr>
2025-10-31 12:31:14 +0000

    Parametrisation and fixtures for atmosphere and core folders (except setup_func_sp in test_lib)

--- smrt/core/test_globalconstants.py
+++ smrt/core/test_globalconstants.py
@@ -1,20 +1,10 @@
 from smrt.core import globalconstants
+import pytest
 
 # Perhaps these are not strictly necessary, but they will provide an extra level of security
 # to warn if these constants have been changed.
 
-
-def test_density_of_ice():
-    assert globalconstants.DENSITY_OF_ICE == 916.7
-
-
-def test_freezing_point():
-    assert globalconstants.FREEZING_POINT == 273.15
-
-
-def test_permittivity_of_air():
-    assert globalconstants.PERMITTIVITY_OF_AIR == 1
-
-
-def test_speed_of_light():
-    assert globalconstants.C_SPEED == 2.99792458e8
+@pytest.mark.parametrize("name, constant, expected",
+                         [("DENSITY_OF_ICE", globalconstants.DENSITY_OF_ICE, 916.7),        ("FREEZING_POINT", globalconstants.FREEZING_POINT, 273.15),        ("PERMITTIVITY_OF_AIR", globalconstants.PERMITTIVITY_OF_AIR, 1),("C_SPEED", globalconstants.C_SPEED, 2.99792458e8)])
+def test_constant_named(name, constant, expected):
+    assert constant == expected, f"{name} expected {expected}, got {constant}"
